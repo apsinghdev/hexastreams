32cdd07fd7162462b5478e3333afaa296463d449
apsinghdev <ajeetpratap517@gmail.com>
2024-09-29 21:13:39 +0530

    feat: setup backend

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+// import router from "./routes/index.js";
+import cors from "cors";
+// import connectDB from "./db.js";
+
+const PORT = 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/v1", router);
+
+try {
+    // await connectDB();
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+} catch (error) {
+    res.status(403).json({ error: "failed connecting to database" });
+}
