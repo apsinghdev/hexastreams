598ebff7258d5f1a9915b4fe281a5f4aa613edce
apsinghdev <ajeetpratap517@gmail.com>
2024-09-29 15:34:30 +0530

    feat: add ChannelSettings component

--- src/components/ChannelSettings.jsx
+++ src/components/ChannelSettings.jsx
@@ -1,6 +1,6 @@
 export const ChannelSettings = () => {
     return (
-        <div>
+        <div className="w-full xl:w-[40rem] xl:h-full h-3/5 md:h-[32rem] mt-7 xl:m-0 md:mt-10 bg-[#0A1E43] rounded-xl">
             
         </div>
     )
--- src/components/PageBody.jsx
+++ src/components/PageBody.jsx
@@ -3,7 +3,7 @@ import { ChannelSettings } from "./ChannelSettings"
 
 export const PageBody = () => {
     return (
-        <div className="flex flex-col lg:flex-row h-full overflow-hidden w-full">
+        <div className="flex flex-col xl:flex-row h-full overflow-hidden w-full xl:space-x-3">
             <EncoderSettings></EncoderSettings>
             <ChannelSettings></ChannelSettings>
         </div>
