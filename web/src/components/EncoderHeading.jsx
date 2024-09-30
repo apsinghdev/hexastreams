bb6eecaa18f1e1784df2a53094daabf39f2f4787
apsinghdev <ajeetpratap517@gmail.com>
2024-09-30 17:41:01 +0530

    fix: fix overflow text

--- web/src/components/EncoderHeading.jsx
+++ web/src/components/EncoderHeading.jsx
@@ -2,7 +2,7 @@ export const EncoderHeading = () => {
     return (
         <div className='flex flex-col items-center space-y-2 md:pt-12'>
             <p className='text-white hidden md:text-3xl font-semibold md:inline'>Set Up Your Live Stream</p>
-            <p className='text-white text-sm text-slate-400 pt-3 md:text-xl lg:text-lg md:p-0'>Copy and paste <span className='hidden md:inline'> RTMP settings</span> into your streaming software.</p>
+            <p className='text-white text-xs text-slate-400 pt-3 md:text-xl lg:text-lg md:p-0'>Copy and paste <span className='hidden md:inline'> RTMP settings</span> into your streaming software.</p>
         </div>
     )
 }
